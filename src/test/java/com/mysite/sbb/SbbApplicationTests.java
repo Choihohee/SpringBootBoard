732d4222d73b4ab3eaa8e179bbd5760d33ee5070
Choihohee <lovelove4927@naver.com>
2024-01-13 02:22:17 +0900

    test: findById 메서드 사용한 데이터조회 테스트

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,11 +1,12 @@
 package com.mysite.sbb;
 
-import com.mysite.sbb.Model.Question;
+import  com.mysite.sbb.Model.Question;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDateTime;
+import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SbbApplicationTests {
@@ -14,17 +15,12 @@ class SbbApplicationTests {
 
 	@Test
 	void testJpa() {
-		Question question1 = new Question();
-		question1.setSubject("sbb가 무엇인가요?");
-		question1.setContent("sbb에 대해서 알고 싶습니다.");
-		question1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(question1);	//첫 번째 질문 저장
+		Optional<Question> oq = this.questionRepository.findById(1);
+		if(oq.isPresent()){
+			Question q = oq.get();
+			assertEquals("sbb가 무엇인가요?", q.getSubject());
+		}
 
-		Question question2 = new Question();
-		question2.setSubject("스프링부트 모델 질문입니다");
-		question2.setContent("id는 자동으로 생성되나요?");
-		question2.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(question2);	//두 번째 질문 저장
 	}
 
 }
